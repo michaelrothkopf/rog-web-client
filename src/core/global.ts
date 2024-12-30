7fdfba18479d4e50e7941e480c08a56990494239
michaelrothkopf <michaelhrothkopf@gmail.com>
2024-12-30 02:20:21 -0500

    production routes

--- src/core/global.ts
+++ src/core/global.ts
@@ -1,6 +1,6 @@
 import { Socket } from 'socket.io-client';
 
-export const GLOBAL_BASE_URL = 'http://192.168.1.156:8148';
+export const GLOBAL_BASE_URL = 'http://reroutemr.zapto.org:8148';
 export const AUTH_LOGIN_PATH = GLOBAL_BASE_URL + '/auth/login';
 export const AUTH_SIGNUP_PATH = GLOBAL_BASE_URL + '/auth/signup';
 export const FRIEND_LIST_PATH = GLOBAL_BASE_URL + '/friends';
@@ -14,7 +14,7 @@ export const HEADERS = new Headers();
 export const BODY_HEADERS = new Headers();
 BODY_HEADERS.set('Content-Type', 'application/json');
 
-export const SOCKET_URL_BASE = 'http://192.168.1.156:8148/';
+export const SOCKET_URL_BASE = 'http://reroutemr.zapto.org:8148/';
 export const SOCKET_CONNECTION_PATH = '/live/socket.io'
 
 interface GlobalState {
