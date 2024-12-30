941bb7f979d16d2d82a6fe086d982c52d93f6ccc
michaelrothkopf <michaelhrothkopf@gmail.com>
2024-12-30 02:42:31 -0500

    config fixes and comment unused code

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './pages/global/index.css';
 import App from './App.tsx';
-import TestApp from './TestApp.tsx';
+// import TestApp from './TestApp.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
